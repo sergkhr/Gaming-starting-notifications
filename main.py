c552226fde4071dd4af57f4046e931aba02204f1
sergkhr <serg.khrechko@gmail.com>
2026-06-01 15:01:09 +0300

    added tests
    TODO chtck if it builds n runs

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import argparse
+
+from tests.run_tests import run_tests
+
+
 def main():
-    print("Notifier started")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Run test cases"
+    )
+
+    args = parser.parse_args()
+
+    if args.test:
+        run_tests()
+        return
+
+    print("Notifier (nothing so far) started")
 
 
 if __name__ == "__main__":
---
+++ tests/test_cases.py
@@ -0,0 +1,53 @@
+TEST_CASES = [
+
+    {
+        "name": "simple_dota_invite",
+        "text": "го дота"
+    },
+
+    {
+        "name": "minecraft_question",
+        "text": "когда в майн?"
+    },
+
+    {
+        "name": "dota_with_urgency",
+        "text": "через 20 минут в доту"
+    },
+
+    {
+        "name": "vibe_invite",
+        "text": "есть вайб на доту после 12?"
+    },
+
+    {
+        "name": "need_one_player",
+        "text": "нужен еще один в леталку"
+    },
+
+    {
+        "name": "discord_gathering",
+        "text": "го в дс на факторио"
+    },
+
+    {
+        "name": "positive_response",
+        "text": "я в деле"
+    },
+
+    {
+        "name": "soon_response",
+        "text": "буду через 15 минут"
+    },
+
+    {
+        "name": "negative_response",
+        "text": "не могу сегодня"
+    },
+
+    {
+        "name": "game_news",
+        "text": "вышел новый патч для доты"
+    },
+
+]
